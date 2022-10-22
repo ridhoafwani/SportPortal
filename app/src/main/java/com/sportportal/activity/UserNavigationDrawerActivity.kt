2db6905c06dae1470fa5feb998c2414dfdfab45f
ridhoafwani <zikrurridhoafwani@gmail.com>
2022-10-22 12:46:54 +0800

    New Build
    New Build & Signed

--- app/src/main/java/com/sportportal/activity/UserNavigationDrawerActivity.kt
+++ app/src/main/java/com/sportportal/activity/UserNavigationDrawerActivity.kt
@@ -16,7 +16,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.sportportal.R
 import com.sportportal.R.id
 
-
 @Suppress("DEPRECATION")
 open class UserNavigationDrawerActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -24,20 +23,16 @@ open class UserNavigationDrawerActivity : AppCompatActivity(), View.OnClickListe
     private lateinit var container : FrameLayout
     private val auth : FirebaseAuth get() = Login.user
 
-
     override fun setContentView(view: View?) {
         drawerLayout = layoutInflater.inflate(R.layout.activity_user_navigation_drawer, null) as DrawerLayout
         container = drawerLayout.findViewById(id.activityContainer)
         view.removeSelf()
         container.addView(view)
 
-
         super.setContentView(drawerLayout)
 
         val toolbar : Toolbar = drawerLayout.findViewById(id.toolbar)
         setSupportActionBar(toolbar)
-
-//        var navigationView : NavigationView = drawerLayout.findViewById(id.nav_view)
 
         val toggle = ActionBarDrawerToggle(this, drawerLayout,toolbar,
                     R.string.openDrawer, R.string.closeDrawer)
@@ -46,22 +41,16 @@ open class UserNavigationDrawerActivity : AppCompatActivity(), View.OnClickListe
         val lapangan : LinearLayout = drawerLayout.findViewById(id.lapangan_menu)
         val profile : LinearLayout = drawerLayout.findViewById(id.profile_menu)
         val pesanan : LinearLayout = drawerLayout.findViewById(id.pesanan_menu)
-//        val penyedia : LinearLayout = drawerLayout.findViewById(id.penyedia)
-//        val admin : LinearLayout = drawerLayout.findViewById(id.admin)
 
         logout.setOnClickListener(this)
         lapangan.setOnClickListener(this)
         profile.setOnClickListener(this)
         pesanan.setOnClickListener(this)
-//        penyedia.setOnClickListener(this)
-//        admin.setOnClickListener(this)
 
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-
     }
-
 
     override fun onClick(v: View?) {
         when (v?.id) {
@@ -95,16 +84,6 @@ open class UserNavigationDrawerActivity : AppCompatActivity(), View.OnClickListe
                 goToProfile()
 
             }
-
-//            id.penyedia ->{
-//                goToPenyedia()
-//
-//            }
-//
-//            id.admin ->{
-//                goToAdmin()
-//            }
-
         }
     }
 
@@ -131,18 +110,6 @@ open class UserNavigationDrawerActivity : AppCompatActivity(), View.OnClickListe
         startActivity(intent)
         drawerLayout.close()
     }
-
-//    private fun goToPenyedia(){
-//        val intent = Intent(baseContext,PenyediaLapanganList::class.java)
-//        startActivity(intent)
-//        drawerLayout.close()
-//    }
-//
-//    private fun goToAdmin(){
-//        val intent = Intent(baseContext,AdminLapanganList::class.java)
-//        startActivity(intent)
-//        drawerLayout.close()
-//    }
 
     private fun View?.removeSelf() {
         this ?: return
